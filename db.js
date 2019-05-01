54ca7ec83a53866a00457e330ce4f26a64ab60c1
Albert Saputra <a.albert.saputra@gmail.com>
2019-04-30 17:53:45 -0700

    [first] seeding func & db connection

---
+++ db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/airbnbDesc', { useNewUrlParser: true , useFindAndModify: false, useCreateIndex: true })
+  .then(() => {console.log('connected to mongoDB.')});
+
+const propertySchema = new mongoose.Schema ({
+  id: { type: Number, index: true, unique: true, dropDups: true },
+  propertyInfo_propType: String,
+  propertyInfo_title: String,
+  propertyInfo_location: String,
+  propertyInfo_numGuests: { type: Number, index: true},
+  beds_quantity: Number,
+  amenities: {basic: Array, notIncluded: Array, iconUrl: Array},
+  numBaths: Number,
+  host_name: { type: String }, 
+  host_pictureUrl: String,
+  summary: Array
+});
+
+var Property = mongoose.model('housedescriptions', propertySchema);
+
